7f73251ff52c42789eb746bcd5167ca18c52be17
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-25 09:26:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+VIX_TICKER = "^VIX"
+SPY_TICKER = "SPY"
+
+RSS_FEEDS = [
+    "https://www.moneycontrol.com/rss/latestnews.xml",
+    "https://finance.yahoo.com/rss/"
+]
+
+LOOKBACK_PERIOD = "5d"
+INTERVAL = "5m"
